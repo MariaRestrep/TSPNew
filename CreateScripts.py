c85e4c806d8df8380e07c346db7ae60802ea95f0
MariaRestrep <maisa26@gmail.com>
2023-01-17 19:47:08 +0100

    update script

--- CreateScripts.py
+++ CreateScripts.py
@@ -35,7 +35,7 @@ module purge
 module load intel/2016.3.210
 module load intel/mkl/64/2016.3.210
 module load intel/mpi/2016.3.210
-module load python/2.7.12
+module load python/3.7.4
 module load intel/mkl/64/2017.4.196
 module load gcc/12.1.0
 
